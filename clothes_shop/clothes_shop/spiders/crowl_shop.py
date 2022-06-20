ee2184bc264b173b27027852fa08fa0904555a5d
warshtayner <warshtayner55@gmail.com>
2022-06-20 16:13:25 +0300

    crowl_OK

---
+++ clothes_shop/clothes_shop/spiders/crowl_shop.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class CrowlShopSpider(CrawlSpider):
+    name = 'crowl_shop'
+    allowed_domains = ['scrapingclub.com']
+    start_urls = ['http://scrapingclub.com/exercise/list_basic/']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//a[contains(text(), 'Next')]"), follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//h4[@class='card-title']/a"), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = {
+            'name' : response.xpath("//div[@class='card-body']/h3/text()").get(),
+            'price': response.xpath("//div[@class='card-body']/h4/text()").get(),
+            'description' : response.xpath("//div[@class='card-body']/p/text()").get(),
+            'img' : "https://scrapingclub.com/" + response.xpath("//img[@class='card-img-top img-fluid']/@src").get()
+        }
+        return item
