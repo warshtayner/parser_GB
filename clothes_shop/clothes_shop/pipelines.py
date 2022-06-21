82113bea4f967771baf9436bde091ae69a228abb
warshtayner <warshtayner55@gmail.com>
2022-06-21 14:18:17 +0300

    correct

--- clothes_shop/clothes_shop/pipelines.py
+++ clothes_shop/clothes_shop/pipelines.py
@@ -5,9 +5,36 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
+from venv import create
 from itemadapter import ItemAdapter
 
 
 class ClothesShopPipeline:
+    def open_spider(self, spider):
+        self.conection = sqlite3.connect('scrapy_shop')
+        self.cursor = self.conection.cursor()
+
     def process_item(self, item, spider):
-        return item
+        create_query = """
+            CREATE TABLE IF NOT EXISTS shop{
+                title TEXT,
+                image TEXT,
+                price TEXT
+            }
+        """
+
+        self.cursor.execute(create_query)
+        self.conection.commit()
+
+        save_query= """
+            INSERT INTO shop{title, image, price}
+            VALUES {?,?,?}
+        """
+
+        self.cursor.execute(save_query, (item.get('title', 0), item.get('image'), item.get('price')))
+        self.conection.commit()
+
+
+    def close_spider(self, spider):
+        self.conection.close()
--- clothes_shop/clothes_shop/settings.py
+++ clothes_shop/clothes_shop/settings.py
@@ -63,9 +63,9 @@ FEED_EXPORT_ENCODING = "UTF-8"
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'clothes_shop.pipelines.ClothesShopPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'clothes_shop.pipelines.ClothesShopPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
